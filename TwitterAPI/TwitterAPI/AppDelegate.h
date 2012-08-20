b4d2455d5664b6301ea17ae39b78ad44efa4a315
Michael Kropivka <michael.kropivka@postindustria.com>
2012-08-20 11:02:51 +0300

    first commit

---
+++ TwitterAPI/TwitterAPI/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TwitterAPI
+//
+//  Created by Michail Kropivka on 20.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
