b3d5d38f204f948cd7a49625528735f9e2c6ab0e
Michael Kropivka <michael.kropivka@postindustria.com>
2012-08-20 17:23:47 +0300

    example with facebook

---
+++ FacebookAPI/FacebookAPI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FacebookAPI
+//
+//  Created by Michail Kropivka on 20.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
